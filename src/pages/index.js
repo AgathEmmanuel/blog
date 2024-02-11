87e66d3800a29232930e659e0ee52d8a3defd63f
agathemmanuel <agathemmanuel00@gmail.com>
2024-02-11 19:26:41 +0530

    adding updates

--- src/pages/index.js
+++ src/pages/index.js
@@ -14,7 +14,7 @@ function HomepageHeader() {
       <div className="container">
         <h1 className="hero__title">{siteConfig.title}</h1>
         <p className="hero__subtitle">{siteConfig.tagline}</p>
-        <div className={styles.buttons}>
+        <div className={styles.welcomeButton}>
           <Link
             className="button button--secondary button--lg"
             to="/docs/welcome">
